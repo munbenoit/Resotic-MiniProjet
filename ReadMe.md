189486296cad27779ab7f14bb1d161415c6cdeaf
munbenoit <mun.benoit@gmail.com>
2018-11-23 22:12:53 +0100

    Adding a navbar

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Mini-Projet Resotic
+Réalisation d'un projet pour l'entreprise Resotic avec une limite de temps d'environ 2 jours.
+
+## Objectifs
+- Création d'un outil de visualisation de l'organigramme d'une entreprise
+- Formulaire d'ajout d'un nouvel employé dans l'organigramme
+
+## Outils
+- Framework Ionic
+- API randomuser
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
