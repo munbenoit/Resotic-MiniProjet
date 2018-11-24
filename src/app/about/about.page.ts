260f852e61c4739a36d771c3e5f9b1f897752870
munbenoit <mun.benoit@gmail.com>
2018-11-24 10:18:17 +0100

    Add Cards for users in About section

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage{
+  users$: Observable<any>;
+  constructor(private httpClient: HttpClient){
+    this.users$ = this.httpClient.get('https://jsonplaceholder.typicode.com/users');
+    this.users$.subscribe(data=>this.users$=data);
+  }
+  /*this.data.getUsers().subscribe(
+    data => this.users$ = data
+  )*/
+
+}
